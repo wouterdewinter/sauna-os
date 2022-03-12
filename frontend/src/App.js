9be0f27c85b9a45496e098ce531ddbaab06a75f4
Wouter de Winter <wouter.de.winter@artificialindustry.com>
2022-03-12 17:58:29 +0100

    import

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <label>
+          <input type={"checkbox"} />
+          Links voor
+        </label>
+        <label>
+          <input type={"checkbox"} />
+          Rechts voor
+        </label>
+      </header>
+    </div>
+  );
+}
+
+export default App;
