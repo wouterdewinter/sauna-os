199393d6ee0b2bd4ea249e5dde829ec83a46619c
Wouter de Winter <wouter.de.winter@artificialindustry.com>
2022-03-13 11:46:52 +0100

    wip

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const Gpio = require('onoff').Gpio;
 const express = require('express')
+const ds18b20 = require("ds18b20");
 
 // tutorial for enabling the one-wire interface:
 // https://www.circuitbasics.com/raspberry-pi-ds18b20-temperature-sensor-tutorial/
 // 28-06201c5a979c
+
+const tempSensorId = "28-06201c5a979c";
+
+// raspberry pi pinout:
+// https://www.raspberrypi-spy.co.uk/2012/06/simple-guide-to-the-rpi-gpio-header-and-pins/
 
 const relais = [
   new Gpio(14, 'out'),
@@ -16,12 +22,19 @@ const relais = [
   new Gpio(7, 'out'),
 ]
 
+const temp = ds18b20.temperatureSync(tempSensorId);
+console.log("temp is:" +temp);
 
 const app = express()
 const port = 8001
 
 app.get('/', (req, res) => {
   res.send('Hello Worlsd!')
+})
+
+app.get('/temp', (req, res) => {
+  const temp = ds18b20.temperatureSync(tempSensorId);
+  res.send('Temp is: '+  temp)
 })
 
 app.get('/set', (req, res) => {
