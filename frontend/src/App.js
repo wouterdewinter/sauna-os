8e58b56ef632f3a498c36c596c08363bd8a54969
Wouter de Winter <wouter.de.winter@artificialindustry.com>
2022-04-09 15:16:45 +0200

    rebuild

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -85,143 +85,145 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Grid container spacing={2} justifyContent={"center"}>
-          <Grid item>
-            <Fab
-              color={status.power === "off" ? "grey" : "primary"}
-              aria-label="add"
-              onClick={togglePower}
-            >
-              <PowerSettingsNewIcon color={"white"} fontSize={"large"} />
-            </Fab>
-          </Grid>
-          <Grid item>
-            <Fab
-              color={status.isLightEnabled ? "primary" : "grey"}
-              aria-label="add"
-              onClick={toggleLight}
-            >
-              <Lightbulb color={"white"} fontSize={"large"} />
-            </Fab>
-          </Grid>
-          <Grid item>
-            <Fab
-              color={status.isColorEnabled ? "primary" : "grey"}
-              aria-label="add"
-              onClick={toggleColor}
-            >
-              <TipsAndUpdates color={"white"} fontSize={"large"} />
-            </Fab>
-          </Grid>
-        </Grid>
-        <br />
-        <Grid container spacing={2} justifyContent={"center"}>
-          <Grid item xs={1}>
-            <Metric
-              label={"Heating"}
-              value={status.power === "heating" ? "yes" : "no"}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <Metric
-              label={"Timer"}
-              value={
-                Math.floor(status.timer / 60) +
-                ":" +
-                `${status.timer % 60}`.padStart(2, "0")
-              }
-            >
-              <Button
-                variant="outlined"
-                size="small"
-                onClick={() => timerReset()}
+        <div style={{ maxWidth: "428px", padding: "10px" }}>
+          <Grid container spacing={2} justifyContent={"center"}>
+            <Grid item>
+              <Fab
+                color={status.power === "off" ? "grey" : "primary"}
+                aria-label="add"
+                onClick={togglePower}
               >
-                Reset
-              </Button>
-            </Metric>
+                <PowerSettingsNewIcon color={"white"} fontSize={"large"} />
+              </Fab>
+            </Grid>
+            <Grid item>
+              <Fab
+                color={status.isLightEnabled ? "primary" : "grey"}
+                aria-label="add"
+                onClick={toggleLight}
+              >
+                <Lightbulb color={"white"} fontSize={"large"} />
+              </Fab>
+            </Grid>
+            <Grid item>
+              <Fab
+                color={status.isColorEnabled ? "primary" : "grey"}
+                aria-label="add"
+                onClick={toggleColor}
+              >
+                <TipsAndUpdates color={"white"} fontSize={"large"} />
+              </Fab>
+            </Grid>
           </Grid>
-        </Grid>
-        <br />
-        <Grid container spacing={2} justifyContent={"center"}>
-          <Grid item xs={1}>
-            <Metric label={"Target temp"} value={status.targetTemp} />
+          <br />
+          <Grid container spacing={2} justifyContent={"center"}>
+            <Grid item xs={6}>
+              <Metric
+                label={"Heating"}
+                value={status.power === "heating" ? "yes" : "no"}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Metric
+                label={"Timer"}
+                value={
+                  Math.floor(status.timer / 60) +
+                  ":" +
+                  `${status.timer % 60}`.padStart(2, "0")
+                }
+              >
+                <Button
+                  variant="outlined"
+                  size="small"
+                  onClick={() => timerReset()}
+                >
+                  Reset
+                </Button>
+              </Metric>
+            </Grid>
           </Grid>
-          <Grid item xs={1}>
-            <Metric
-              label={"Current temp"}
-              value={Math.round(status.currentTemp)}
-            />
+          <br />
+          <Grid container spacing={2} justifyContent={"center"}>
+            <Grid item xs={6}>
+              <Metric label={"Target temp"} value={status.targetTemp} />
+            </Grid>
+            <Grid item xs={6}>
+              <Metric
+                label={"Current temp"}
+                value={Math.round(status.currentTemp)}
+              />
+            </Grid>
           </Grid>
-        </Grid>
 
-        <br />
+          <br />
 
-        <Grid container spacing={2} justifyContent={"center"}>
-          <Grid item>
-            <Fab
-              size={"small"}
-              onClick={() => setTarget(status.targetTemp - 1)}
-            >
-              <Remove color={"white"} fontSize={"small"} />
-            </Fab>
+          <Grid container spacing={2} justifyContent={"center"}>
+            <Grid item>
+              <Fab
+                size={"small"}
+                onClick={() => setTarget(status.targetTemp - 1)}
+              >
+                <Remove color={"white"} fontSize={"small"} />
+              </Fab>
+            </Grid>
+            <Grid item>
+              <Fab onClick={() => setTarget(status.targetTemp - 5)}>
+                <Remove color={"white"} fontSize={"medium"} />
+              </Fab>
+            </Grid>
+            <Grid item>
+              <Fab onClick={() => setTarget(status.targetTemp + 5)}>
+                <Add color={"white"} fontSize={"medium"} />
+              </Fab>
+            </Grid>
+            <Grid item>
+              <Fab
+                onClick={() => setTarget(status.targetTemp + 1)}
+                size={"small"}
+              >
+                <Add color={"white"} fontSize={"small"} />
+              </Fab>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Fab onClick={() => setTarget(status.targetTemp - 5)}>
-              <Remove color={"white"} fontSize={"medium"} />
-            </Fab>
-          </Grid>
-          <Grid item>
-            <Fab onClick={() => setTarget(status.targetTemp + 5)}>
-              <Add color={"white"} fontSize={"medium"} />
-            </Fab>
-          </Grid>
-          <Grid item>
-            <Fab
-              onClick={() => setTarget(status.targetTemp + 1)}
-              size={"small"}
-            >
-              <Add color={"white"} fontSize={"small"} />
-            </Fab>
-          </Grid>
-        </Grid>
 
-        <br />
-        <FormControlLabel
-          control={
-            <Switch
-              checked={status.panelsEnabled[0]}
-              onChange={() => toggle(0)}
-            />
-          }
-          label="Front right"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              checked={status.panelsEnabled[1]}
-              onChange={() => toggle(1)}
-            />
-          }
-          label="Back right + bottom"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              checked={status.panelsEnabled[2]}
-              onChange={() => toggle(2)}
-            />
-          }
-          label="Front left"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              checked={status.panelsEnabled[3]}
-              onChange={() => toggle(3)}
-            />
-          }
-          label="Back left"
-        />
+          <br />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={status.panelsEnabled[0]}
+                onChange={() => toggle(0)}
+              />
+            }
+            label="Front right"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={status.panelsEnabled[1]}
+                onChange={() => toggle(1)}
+              />
+            }
+            label="Back right + bottom"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={status.panelsEnabled[2]}
+                onChange={() => toggle(2)}
+              />
+            }
+            label="Back left"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={status.panelsEnabled[3]}
+                onChange={() => toggle(3)}
+              />
+            }
+            label="Front left"
+          />
+        </div>
       </header>
     </div>
   );
